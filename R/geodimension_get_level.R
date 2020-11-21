af9249fac63379cfec6c471407a96f7ad57efe53
Jose Samos <jsamos@ugr.es>
2020-11-21 19:07:11 +0100

    Select levels.

--- R/geodimension_get_level.R
+++ R/geodimension_get_level.R
@@ -30,6 +30,49 @@ get_level_names.geodimension <- function(gd) {
   names(gd$geolevel)
 }
 
+# select levels ---------------------------------------------------------
+
+#' Add a level to a dimension
+#'
+#' Add a level to a dimension. This level can then be related to other levels of
+#' the dimension.
+#'
+#' @param gd A `geodimension` object.
+#' @param level_names A vector of names.
+#'
+#' @return A `geodimension` object.
+#'
+#' @family geodimension functions
+#' @seealso
+#'
+#' @examples
+#' library(tidyr)
+#'
+#'
+#' @export
+select_levels <- function(gd, level_names = NULL) {
+  UseMethod("select_levels")
+}
+
+
+#' @rdname select_levels
+#' @export
+select_levels.geodimension <- function(gd, level_names = NULL) {
+  level_names <- unique(level_names)
+  existing_names <- names(gd$geolevel)
+  stopifnot(level_names %in% existing_names)
+  delete <- generics::setdiff(existing_names, level_names)
+  for (del in delete) {
+    gd$geolevel[[del]] <- NULL
+    gd$relation[[del]] <- NULL
+  }
+  for (level in names(gd$relation)) {
+    rel_names <- generics::setdiff(names(gd$relation[[level]]), delete)
+    gd$relation[[level]] <- gd$relation[[level]][, rel_names]
+  }
+  gd
+}
+
 # get level geometries ---------------------------------------------------------------
 
 
@@ -163,7 +206,6 @@ get_level_layer.geodimension <- function(gd,
                                          attributes = FALSE,
                                          inherited = FALSE,
                                          geometry = NULL) {
-browser()
   stopifnot(level_name %in% names(gd$geolevel))
   if (is.null(geometry)) {
     geometry <- names(gd$geolevel[[level_name]]$geometry)[1]
