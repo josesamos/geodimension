39fa75a00f813033760699ab78c05c778473b1b8
Jose Samos <jsamos@ugr.es>
2020-11-22 14:10:39 +0100

    New data set.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' `gd_us_city`
+#'
+#' `geodimension` obtained from vector layers over USA.
+#'
+#' It includes the levels city, county, state, region, division and nation.
+#'
+#' @format A `geodimension`.
+#' @source
+#'   \url{https://www.census.gov}
+"gd_us_city"
+
