2baa9863e8c3e91884d94a66733fce2458036091
josesamos <jsamos@ugr.es>
2023-10-24 21:23:54 +0200

    source code reorganization

--- R/geolevel.R
+++ R/geolevel.R
@@ -15,8 +15,8 @@
 #'
 #' @param name A string, level name.
 #' @param layer A `sf` object.
-#' @param attributes A vector, selected attributes.
-#' @param key A vector, attributes that compose the key.
+#' @param attributes A string vector, selected attributes.
+#' @param key A string vector, attributes that compose the key.
 #'
 #' @return A `geolevel` object.
 #'
@@ -37,7 +37,7 @@ geolevel <-
            attributes = NULL,
            key = NULL) {
     stopifnot("Missing geolevel name." = !is.null(name))
-    stopifnot("layer does not include sf object." = methods::is(layer, "sf"))
+    stopifnot("Layer does not include an sf object." = methods::is(layer, "sf"))
     geometry <- get_geometry(layer)
     if (!(geometry %in% c("polygon", "point", "line"))) {
       stop(sprintf('layer has unsupported geometry: %s.', geometry[1]))
@@ -223,4 +223,122 @@ validate_attributes <- function(defined_attributes, attributes, repeated = FALSE
 surrogate_key_name <- function(name) {
   sprintf("%s_key", snakecase::to_snake_case(name))
 }
+
+
+# empty geometry ----------------------------------------------------------
+
+#' Get empty geometry instances
+#'
+#' Get the instances of the data table that do not have associated geometry for
+#' the specified geometry type.
+#'
+#' @param gl A `geolevel` object.
+#' @param geometry A string, type of geometry of the layer.
+#'
+#' @return A `tibble`.
+#'
+#' @family level definition functions
+#'
+#' @examples
+#' us_state_point <-
+#'   coordinates_to_geometry(layer_us_state,
+#'                           lon_lat = c("intptlon", "intptlat"))
+#'
+#' state <-
+#'   geolevel(name = "state",
+#'            layer = layer_us_state,
+#'            key = c("geoid")) |>
+#'   add_geometry(layer = us_state_point)
+#'
+#' empty_geometry_instances <- state |>
+#'   get_empty_geometry_instances(geometry = "point")
+#'
+#' @export
+get_empty_geometry_instances <- function(gl,
+                                         geometry = NULL) {
+  UseMethod("get_empty_geometry_instances")
+}
+
+
+#' @rdname get_empty_geometry_instances
+#' @export
+get_empty_geometry_instances.geolevel <- function(gl,
+                                                  geometry = NULL) {
+  stopifnot(geometry %in% names(gl$geometry))
+  if (is.null(geometry)) {
+    geometry <- names(gl$geometry)[1]
+  }
+  gl$data[!(gl$data[[1]] %in% gl$geometry[[geometry]][[1]]), ]
+}
+
+# complete point geometry ----------------------------------------------------------
+
+#' Complete point geometry
+#'
+#' In case of having the polygon geometry defined, it obtains the point geometry
+#' from it.
+#'
+#' If the point geometry was already defined, if there are instances with this
+#' geometry empty, it completes them.
+#'
+#' If the geometry of the CRS is not projected, it warns that the calculations
+#' may not be correct. A projected intermediate geometry can be used to perform
+#' the operation, indicating it by the boolean parameter.
+#'
+#' @param gl A `geolevel` object.
+#' @param use_intermediate_projected_crs A boolean.
+#'
+#' @return A `geolevel` object.
+#'
+#' @family level definition functions
+#'
+#' @examples
+#' state <-
+#'   geolevel(name = "state",
+#'            layer = layer_us_state,
+#'            key = c("geoid")) |>
+#'   complete_point_geometry()
+#'
+#' @export
+complete_point_geometry <- function(gl, use_intermediate_projected_crs = FALSE) {
+  UseMethod("complete_point_geometry")
+}
+
+
+#' @rdname complete_point_geometry
+#' @export
+complete_point_geometry.geolevel <- function(gl, use_intermediate_projected_crs = FALSE) {
+  stopifnot("polygon" %in% names(gl$geometry))
+  if ("point" %in% names(gl$geometry)) {
+    layer <- gl$geometry[["polygon"]][!(gl$data[[1]] %in% gl$geometry[["point"]][[1]]), ]
+    # to avoid warning: make the assumption (that the attribute is constant throughout the geometry)
+    sf::st_agr(layer) = "constant"
+    if (use_intermediate_projected_crs) {
+      rest <- layer |>
+        sf::st_transform(crs = 3395)
+    } else {
+      rest <- layer
+    }
+    rest <- rest |>
+      sf::st_point_on_surface() |>
+      sf::st_transform(crs = sf::st_crs(gl$geometry[["point"]]))
+    gl$geometry[["point"]] <- gl$geometry[["point"]] |>
+      tibble::add_row(rest)
+  } else {
+    layer <- gl$geometry[["polygon"]]
+    # to avoid warning: make the assumption (that the attribute is constant throughout the geometry)
+    sf::st_agr(layer) = "constant"
+    if (use_intermediate_projected_crs) {
+      gl$geometry[["point"]] <- layer |>
+        sf::st_transform(crs = 3395) |>
+        sf::st_point_on_surface()  |>
+        sf::st_transform(crs = sf::st_crs(gl$geometry[["polygon"]]))
+    } else {
+      gl$geometry[["point"]] <- layer |>
+        sf::st_point_on_surface()
+    }
+  }
+  gl
+}
+
 
